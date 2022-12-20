ea898f0dc13de0a23d41ce5b39648d149c32eb85
Bolaji Bello <belloblj2@gmail.com>
2022-12-20 13:24:13 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Nav, Bio, Gallery } from './components'
+import './App.css';
+
+
+const App = () => {
+  return ( 
+    <>
+    <Nav />
+    <div className="container">
+    <Bio />
+    <Gallery />
+  </div>
+  </>
+  )
+}
+
+export default App
